5742dcde7dcddcf7d0ab618cfdbd70e7889ac572
rahimbd7 <rahimbd7@gmail.com>
2024-10-30 18:41:10 +0600

    performed all the queries and cross checked also whats these changes are meade by queries

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -54,3 +54,75 @@ VALUES
     ( 'Prisma', 3);
 
 SELECT * FROM courses
+
+
+-- Insert sample data into "enrollment" table
+INSERT INTO enrollment (student_id, course_id)
+VALUES
+    (1, 1),
+    (1, 2),
+    (2, 1),
+    (3, 2);
+
+
+SELECT * FROM enrollment;
+
+-- Query 1: Insert a new student record
+INSERT INTO students (student_name, age, email, frontend_mark, backend_mark, status)
+VALUES ('rahim', 34, 'rahim@rahim.me', 85, 85, NULL);
+
+
+-- Query 2: Retrieve names of students enrolled in the 'Next.js' course
+SELECT s.student_name
+FROM students s
+JOIN enrollment e ON s.student_id = e.student_id
+JOIN courses c ON e.course_id = c.course_id
+WHERE c.course_name = 'Next.js';
+
+
+-- Query 3: Update status of the student with the highest total mark
+UPDATE students
+SET status = 'Awarded'
+WHERE (frontend_mark + backend_mark) = (
+    SELECT MAX(frontend_mark + backend_mark) FROM students
+);
+
+SELECT * FROM students;
+
+
+
+-- Query 4: Delete courses with no enrollments
+DELETE FROM courses
+WHERE course_id NOT IN (SELECT DISTINCT course_id FROM enrollment);
+
+SELECT * FROM courses;
+
+
+-- Query 5: Retrieve names of students using a limit of 2, starting from the 3rd student
+SELECT student_name
+FROM students
+ORDER BY student_id
+LIMIT 2 OFFSET 2;
+
+
+-- Query 6: Retrieve course names and the number of students enrolled in each course
+SELECT c.course_name, COUNT(e.student_id) AS students_enrolled
+FROM courses c
+LEFT JOIN enrollment e ON c.course_id = e.course_id
+GROUP BY c.course_name;
+
+SELECT * FROM enrollment;
+
+
+
+-- Query 7: Calculate the average age of all students
+SELECT round(AVG(age), 2) AS average_age
+FROM students;
+
+
+-- Query 8: Retrieve names of students whose email contains 'example.com'
+SELECT student_name
+FROM students
+WHERE email LIKE '%example.com%';
+
+SELECT * FROM students;
