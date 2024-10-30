0076bd6d1066cfe6644c98aa7a5c6a1e590e6426
rahimbd7 <rahimbd7@gmail.com>
2024-10-30 17:57:03 +0600

    seeded demo data in tables

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,6 +1,9 @@
 -- Create Database
 CREATE DATABASE university_db;
 
+-- Connect to Database
+SELECT current_database();
+
 -- 1. Create the "students" table
 CREATE TABLE students (
     student_id SERIAL PRIMARY KEY,
@@ -27,3 +30,27 @@ CREATE TABLE enrollment (
     student_id INTEGER REFERENCES students(student_id) ON DELETE CASCADE,
     course_id INTEGER REFERENCES courses(course_id) ON DELETE CASCADE
 );
+
+
+
+-- Insert sample data into "students" table
+INSERT INTO students (student_name, age, email, frontend_mark, backend_mark, status)
+VALUES
+    ( 'Sameer', 21, 'sameer@example.com', 48, 60, NULL),
+    ( 'Zoya', 23, 'zoya@example.com', 52, 58, NULL),
+    ( 'Nabil', 22, 'nabil@example.com', 37, 46, NULL),
+    ( 'Rafi', 24, 'rafi@example.com', 41, 40, NULL),
+    ( 'Sophia', 22, 'sophia@example.com', 50, 52, NULL),
+    ( 'Hasan', 23, 'hasan@gmail.com', 43, 39, NULL);
+
+SELECT * FROM students
+
+-- Insert sample data into "courses" table
+INSERT INTO courses (course_name, credits)
+VALUES
+    ( 'Next.js', 3),
+    ( 'React.js', 4),
+    ( 'Databases', 3),
+    ( 'Prisma', 3);
+
+SELECT * FROM courses
